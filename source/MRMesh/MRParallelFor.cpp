6a3f286527f6c299e12181e38f5a890346abbb8e
Artur Sharafutdinov <artur.sharafutdinov@meshinspector.com>
2025-08-04 21:56:19 +0200

    Migrate callback version

--- source/MRMesh/MRParallelFor.cpp
+++ source/MRMesh/MRParallelFor.cpp
@@ -22,4 +22,65 @@ void parallelFor( size_t begin, size_t end, FunctionRef<void ( void*, size_t )> 
     } );
 }
 
+bool parallelFor( size_t begin, size_t end, FunctionRef<void ( void*, size_t )> f, FunctionRef<void* ()> ctx,
+    ProgressCallback cb, size_t reportProgressEvery )
+{
+    if ( !cb )
+    {
+        parallelFor( begin, end, f, ctx );
+        return true;
+    }
+
+    const auto size = end - begin;
+    if ( size <= 0 )
+        return true;
+
+    TbbThreadMutex callingThreadMutex;
+    std::atomic<bool> keepGoing{ true };
+
+    // avoid false sharing with other local variables
+    // by putting processedBits in its own cache line
+    constexpr int hardware_destructive_interference_size = 64;
+    struct alignas(hardware_destructive_interference_size) S
+    {
+        std::atomic<size_t> processed{ 0 };
+    } s;
+    static_assert( alignof(S) == hardware_destructive_interference_size );
+    static_assert( sizeof(S) == hardware_destructive_interference_size );
+
+    tbb::parallel_for( tbb::blocked_range( begin, end ), [&] ( const tbb::blocked_range<size_t>& range )
+    {
+        const auto callingThreadLock = callingThreadMutex.tryLock();
+        const bool report = cb && callingThreadLock;
+        size_t myProcessed = 0;
+
+        void* ctx_ = ctx();
+        for ( auto i = range.begin(); i < range.end(); ++i )
+        {
+            if ( !keepGoing.load( std::memory_order_relaxed ) )
+                break;
+
+            f( ctx_, i );
+
+            if ( ( ++myProcessed % reportProgressEvery ) == 0 )
+            {
+                if ( report )
+                {
+                    if ( !cb( float( myProcessed + s.processed.load( std::memory_order_relaxed ) ) / float( size ) ) )
+                        keepGoing.store( false, std::memory_order_relaxed );
+                }
+                else
+                {
+                    s.processed.fetch_add( myProcessed, std::memory_order_relaxed );
+                    myProcessed = 0;
+                }
+            }
+        }
+        const auto total = myProcessed + s.processed.fetch_add( myProcessed, std::memory_order_relaxed );
+        if ( report && !cb( float( total ) / float( size ) ) )
+            keepGoing.store( false, std::memory_order_relaxed );
+    } );
+    return keepGoing.load( std::memory_order_relaxed );
+}
+
 } // namespace MR
--- source/MRMesh/MRParallelFor.h
+++ source/MRMesh/MRParallelFor.h
@@ -11,81 +11,6 @@
 namespace MR
 {
 
-namespace Parallel
-{
-
-template <typename I, typename CM, typename F>
-void For( I begin, I end, const CM & callMaker, F && f )
-{
-    tbb::parallel_for( tbb::blocked_range( begin, end ),
-        [&] ( const tbb::blocked_range<I>& range )
-    {
-        auto c = callMaker();
-        for ( I i = range.begin(); i < range.end(); ++i )
-            c( f, i );
-    } );
-}
-
-template <typename I, typename CM, typename F>
-bool For( I begin, I end, const CM & callMaker, F && f, ProgressCallback cb, size_t reportProgressEvery = 1024 )
-{
-    if ( !cb )
-    {
-        For( begin, end, callMaker, std::forward<F>( f ) );
-        return true;
-    }
-    const auto size = end - begin;
-    if ( size <= 0 )
-        return true;
-
-    TbbThreadMutex callingThreadMutex;
-    std::atomic<bool> keepGoing{ true };
-
-    // avoid false sharing with other local variables
-    // by putting processedBits in its own cache line
-    constexpr int hardware_destructive_interference_size = 64;
-    struct alignas(hardware_destructive_interference_size) S
-    {
-        std::atomic<size_t> processed{ 0 };
-    } s;
-    static_assert( alignof(S) == hardware_destructive_interference_size );
-    static_assert( sizeof(S) == hardware_destructive_interference_size );
-
-    tbb::parallel_for( tbb::blocked_range( begin, end ),
-        [&] ( const tbb::blocked_range<I>& range )
-    {
-        const auto callingThreadLock = callingThreadMutex.tryLock();
-        const bool report = cb && callingThreadLock;
-        size_t myProcessed = 0;
-        auto c = callMaker();
-        for ( I i = range.begin(); i < range.end(); ++i )
-        {
-            if ( !keepGoing.load( std::memory_order_relaxed ) )
-                break;
-            c( f, i );
-            if ( ( ++myProcessed % reportProgressEvery ) == 0 )
-            {
-                if ( report )
-                {
-                    if ( !cb( float( myProcessed + s.processed.load( std::memory_order_relaxed ) ) / size ) )
-                        keepGoing.store( false, std::memory_order_relaxed );
-                }
-                else
-                {
-                    s.processed.fetch_add( myProcessed, std::memory_order_relaxed );
-                    myProcessed = 0;
-                }
-            }
-        }
-        const auto total = myProcessed + s.processed.fetch_add( myProcessed, std::memory_order_relaxed );
-        if ( report && !cb( float( total ) / size ) )
-            keepGoing.store( false, std::memory_order_relaxed );
-    } );
-    return keepGoing.load( std::memory_order_relaxed );
-}
-
-} //namespace Parallel
-
 /// \addtogroup BasicGroup
 /// \{
 
@@ -93,41 +18,8 @@ MRMESH_API void parallelFor( size_t begin, size_t end, FunctionRef<void ( size_t
 
 MRMESH_API void parallelFor( size_t begin, size_t end, FunctionRef<void ( void*, size_t )> f, FunctionRef<void* ()> ctx );
 
-template <typename I, typename F>
-inline auto ParallelFor( I begin, I end, F && f )
-{
-    return parallelFor( begin, end, [&] ( size_t i )
-    {
-        std::forward<F>( f )( I( i ) );
-    } );
-}
-
-template <typename I, typename L, typename F>
-inline auto ParallelFor( I begin, I end, tbb::enumerable_thread_specific<L> & e, F && f )
-{
-    return parallelFor( begin, end, [&] ( void* ctx, size_t i )
-    {
-        std::forward<F>( f )( I( i ), *(L*)ctx );
-    }, [&]
-    {
-        return &e.local();
-    } );
-}
-
-template <typename T, typename F>
-inline auto ParallelFor( const std::vector<T> & v, F && f )
-{
-    return parallelFor( size_t(0), v.size(), std::forward<F>( f ) );
-}
-
-template <typename T, typename I, typename F>
-inline auto ParallelFor( const Vector<T, I> & v, F && f )
-{
-    return ParallelFor( v.beginId(), v.endId(), [&] ( size_t i )
-    {
-        std::forward<F>( f )( I( i ) );
-    } );
-}
+MRMESH_API bool parallelFor( size_t begin, size_t end, FunctionRef<void ( void*, size_t )> f, FunctionRef<void* ()> ctx,
+    ProgressCallback cb, size_t reportProgressEvery = 1024 );
 
 /// executes given function f for each span element [begin, end);
 /// optional parameters after f: ProgressCallback cb, size_t reportProgressEvery = 1024 for periodic progress report
@@ -135,7 +27,23 @@ inline auto ParallelFor( const Vector<T, I> & v, F && f )
 template <typename I, typename F, typename ...Args>
 inline auto ParallelFor( I begin, I end, F && f, Args && ... args )
 {
-    return Parallel::For( begin, end, Parallel::CallSimplyMaker{}, std::forward<F>( f ), std::forward<Args>( args )... );
+    if constexpr ( sizeof...( args ) == 0 )
+    {
+        return parallelFor( begin, end, [&] ( size_t i )
+        {
+            std::forward<F>( f )( I( i ) );
+        } );
+    }
+    else
+    {
+        return parallelFor( begin, end, [&] ( void*, size_t i )
+        {
+            std::forward<F>( f )( I( i ) );
+        }, [&]
+        {
+            return nullptr;
+        }, std::forward<Args>( args )... );
+    }
 }
 
 /// executes given function f for each span element [begin, end)
@@ -145,7 +53,13 @@ inline auto ParallelFor( I begin, I end, F && f, Args && ... args )
 template <typename I, typename L, typename F, typename ...Args>
 inline auto ParallelFor( I begin, I end, tbb::enumerable_thread_specific<L> & e, F && f, Args && ... args )
 {
-    return Parallel::For( begin, end, Parallel::CallWithTLSMaker<L>{ e }, std::forward<F>( f ), std::forward<Args>( args )... );
+    return parallelFor( begin, end, [&] ( void* ctx, size_t i )
+    {
+        std::forward<F>( f )( I( i ), *(L*)ctx );
+    }, [&]
+    {
+        return &e.local();
+    }, std::forward<Args>( args )... );
 }
 
 /// executes given function f for each vector element in parallel threads;
